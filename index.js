ad677fad6d331184c165692a4434efa04850ee13
Muhammad Rehman Rafi <153813023+M-Rehmans@users.noreply.github.com>
2026-08-19 15:25:56 -0300

    f

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // $(document).keypress(function(e){
 //     $('h1').text(e.key);
 // });
-$('.before').before("<h3>Before</h3>")
-$('.after').after("<h3>After</h3>")
-$('.append').append("<h3>Append</h3>")
-$('.prepend').prepend("<h3>Prepend</h3>")
+$('.before').before("<h3>Before</h3>");
+$('.after').after("<h3>After</h3>");
+$('.append').append("<h3>Append</h3>");
+$('.prepend').prepend("<h3>Prepend</h3>");
